94c4b382c4ee76692dcc1a2a269a38f9120d2e3f
aletax30 <ale.tassancaser@gmail.com>
2024-02-18 18:22:43 +0100

    tailwind fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-	return <div className="bg-slate-500 text-red-500">prova</div>;
+	return <div className="text-red-500">prova</div>;
 }
